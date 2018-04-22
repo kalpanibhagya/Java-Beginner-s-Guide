654f987ebc64cf2e70b1e12ac78f85034e94c9f8
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2018-04-22 16:41:43 +0530

    Tutorial 03 - Objects & Classes

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person { 
+	public Person(String name) { 
+		// This constructor has one parameter, name. 
+		System.out.println("This person's name is :" + name ); 
+	} 
+
+	public static void main(String []args) { 
+		// Following statement would create an object person 
+		Person person = new Person( "John" ); 
+	} 
+
+}
+

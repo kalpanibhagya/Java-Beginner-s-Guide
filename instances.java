1772c799185632eaf2c1343fef40d5bc0eea20c0
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2018-05-07 21:04:36 +0530

    Tutorial 5 - Operators

---
+++ instances.java
@@ -0,0 +1,11 @@
+public class instances {
+
+   public static void main(String args[]) {
+
+      String name = "Jane";
+
+      // following will return true since name is type of String
+      boolean result = name instanceof String;
+      System.out.println( result );
+   }
+}
